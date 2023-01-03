b4490819cbdd153173a9f3daaddf0e267749693d
Fran249 <Elsilencio_F@hotmail.com>
2023-01-03 14:05:05 -0300

    14/05

---
+++ patio-bus/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+      <CartaCompVue/>
+  </div>
+</template>
+
+<script>
+
+import CartaCompVue from '@/components/CartaComp.vue';
+export default {
+  name: 'HomeView',
+  components: {
+    CartaCompVue,
+  }
+}
+</script>
