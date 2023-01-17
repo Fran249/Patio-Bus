2c864693a6412ee85f62c3cdc0baea0cd04c5ed1
Fran249 <Elsilencio_F@hotmail.com>
2023-01-17 13:15:25 -0300

    Update HomeView.vue

--- patio-bus/src/views/HomeView.vue
+++ patio-bus/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+<<<<<<< HEAD
     <NavBar/>
 
 
@@ -9,13 +10,28 @@
 <script>
   import NavBar from '@/components/NavBar.vue';
 
+=======
+  
+<FooterVue />
+</div>
+</template>
+
+<script>
+ 
+  import FooterVue from '@/components/Footer.vue';
+>>>>>>> 60a07d1f41e2867c5811d29bb64a3f2726c7179b
   export default {
     name: 'HomeView',
     data: ()=>({
       width: window.innerWidth
     }),
+<<<<<<< HEAD
     components: {
       NavBar
+=======
+    components: {   
+      FooterVue
+>>>>>>> 60a07d1f41e2867c5811d29bb64a3f2726c7179b
     },
   }
 </script>
