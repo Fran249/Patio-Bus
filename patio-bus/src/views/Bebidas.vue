eb3a25ffc340b5034439416ef78787b015b59840
AgustinRoveta <118941001+AgustinRoveta@users.noreply.github.com>
2023-01-17 10:33:29 -0300

    17/1
    vistas creadas

---
+++ patio-bus/src/views/Bebidas.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: '',
+    data: () => ({
+        width: window.innerWidth
+    }),
+    components: {
+
+    },
+}
+</script>
