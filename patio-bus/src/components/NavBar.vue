40fe5d7a13d44db5a629597084df949f5382d019
Fran249 <Elsilencio_F@hotmail.com>
2023-01-27 13:18:34 -0300

    Update NavBar.vue

--- patio-bus/src/components/NavBar.vue
+++ patio-bus/src/components/NavBar.vue
@@ -69,7 +69,7 @@
                             <CarritoVue />
 
                     </v-dialog>
-                    <v-dialog>
+                    <v-dialog v-model="ingreso">
                         <IngresoComp/>
                     </v-dialog>
                     <v-dialog>
@@ -86,6 +86,7 @@ export default {
     data: () => ({
         percentage: "600",
         carrito: store.state.carrito,
+        ingreso: true,
     }),
     mounted() {
         window.addEventListener("scroll", () => {
@@ -139,7 +140,6 @@ export default {
             },
         },
     },
-    components: { RegistroComp }
 }
 
 
