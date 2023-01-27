244445e7fe3767b8c7f7526ac715a35c2d7f0d92
Fran249 <Elsilencio_F@hotmail.com>
2023-01-27 13:12:10 -0300

    Ingreso Registro

---
+++ patio-bus/src/components/RegistroComp.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h3>registro</h3>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'RegistroComp',
+    data: ()=>({
+
+    }),
+    methods:{
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
