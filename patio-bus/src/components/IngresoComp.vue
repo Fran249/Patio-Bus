caaf345c5394216f0b2bc1de49cb680652d105de
Fran249 <Elsilencio_F@hotmail.com>
2023-01-27 13:35:40 -0300

    27/1

--- patio-bus/src/components/IngresoComp.vue
+++ patio-bus/src/components/IngresoComp.vue
@@ -1,7 +1,36 @@
 <template>
-    <div>
-        <h3>Ingreso</h3>
-    </div>
+<div class="form-container">
+    <form @submit.prevent="ingresoUsuario({email:email, password:pass})" class="form">
+          <v-text-field
+              type="email"
+              placeholder="E-mail"
+              v-model="email"
+              filled
+              full-width
+              append-icon="mdi-email"
+              class="email"
+              color="grey"
+          ></v-text-field>
+          <v-text-field
+              placeholder="Contraseña"
+              v-model="pass"
+              filled
+              color="grey"
+              class="password"
+              :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="() => (value = !value)"
+              :type="value ? 'password' : 'text'"
+          ></v-text-field>
+            <a @click="ingreso = false" class="d-flex justify-end a-withP"><p>¿Olvidaste tu contraseña?</p></a>
+     
+            <v-btn width="70%" type="submit" class="ml-15" color="#febf2c">
+                <p class="mt-4 p-v-btn" >
+                    ACCEDER
+                </p>
+            </v-btn>
+      </form>
+      <p>{{error}}</p>
+</div>
 </template>
 
 
@@ -9,7 +38,12 @@
 export default {
     name: 'IngresoComp',
     data: ()=>({
-
+        ingreso: true,
+            sendEmail: false,
+            email: '',
+            pass: '',
+            value: String,
+            width : window.innerWidth
     }),
     methods:{
 
@@ -21,4 +55,7 @@ export default {
 
 <style lang="scss" scoped>
 
+.form-container{
+    padding: 25px;
+}
 </style>
